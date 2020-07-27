a95c3f555b201919dd66284dbe0f5c148cc21d2d
Archana Krishnan <a.krishnan@cliqdigital.com>
2020-07-27 17:19:54 +0200

    Script to automate creating snapshot

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # snapshotalyzer-30000
+--24/07/2020
 A demo project to manage AWS EC2 instance
 
 ## About
@@ -11,7 +12,6 @@ shotty uses the Configuration file created by the AWS Cli. eg
 'aws configure -- profile shotty'
 
 ## Running
-
 'pipenv run python shotty/shotty.py <command> <--project=PROJECT>'
 *command* is a list,start,stop or help
 *project* is optional
@@ -19,4 +19,15 @@ shotty uses the Configuration file created by the AWS Cli. eg
 eg: pipenv run  python shotty/shotty.py start --project='xxxxx'
     pipenv run  python shotty/shotty.py stop --project='xxxxx'
     pipenv run  python shotty/shotty.py list --project='xxxxx'
-    pipenv run  python shotty/shotty.py --help 
+    pipenv run  python shotty/shotty.py --help
+
+--27/07/2020
+## Running
+    'pipenv run python shotty/shotty.py <command> <subcommand> <--project=PROJECT>'
+    *command* instances,snapshots,volume or help
+    *sub command* list,create,start,stop
+    *project* is optional
+
+  eg: pipenv run  python shotty/shotty.py instances list
+      pipenv run  python shotty/shotty.py snapshots list
+      pipenv run  python shotty/shotty.py instances snapshots --project Valkyrie
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -13,23 +13,98 @@ def filter_instances(project):
     else:
         instances = ec2.instances.all()
     return instances
-#Commands can be attached to other commands of type Group list,stop,start.
+#Main Command group for snapshot,volumes,instances.
 @click.group()
+def cli():
+    """Shotty Manages snapshots"""
+#Command Group for Sanpshot to list .
+@cli.group('snapshots')
+def snapshots():
+    """Commands for Snapshots"""
+@snapshots.command('list')
+# arguments are passed as parameter declarations to Option:'Project' is passed.
+@click.option('--project',default=None,
+help="Only snapshot for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 Snapshot"
+    #project is passed to filter out and get list of instance
+    instances = filter_instances(project)
+    #iterate for getting instance,volume and its snapshot details together
+    #eg:id,volume id,instance id,state,progress,start time
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                s.id,
+                v.id,
+                i.id,
+                s.state,
+                s.progress,
+                s.start_time.strftime("%c")
+                )))
+    return
+#Command Group for volumes to list.
+@cli.group('volumes')
+def volumes() :
+    """Commands for Volumes"""
+#command for 'List'.
+@volumes.command('list')
+# arguments are passed as parameter declarations to Option:'Project' is passed.
+@click.option('--project',default=None,
+help="Only Volume for project (tag Project:<name>)")
+def list_volume(project):
+    "List EC2 Volume"
+    #project is passed to filter out and get list of instance
+    instances = filter_instances(project)
+    #iterate for getting instance and volume details together for consistency
+    #eg:id,instance id,state,size,encrypted(boolean)
+    for i in  instances :
+        for v in i.volumes.all():
+            print(",".join((v.id,
+            i.id,
+            v.state,
+            str(v.size)+'GiB',
+            v.encrypted and "encrypted" or "Not encrypted"
+            )))
+    return
+#Commands Group for instances to create snapshot and list,stop,startinstances.
+@cli.group('instances')
 def instances():
     """Commands for instances"""
-#command for 'List'.
+#command for 'creating snapshots.
+@instances.command('snapshots',help="Create snapshots of all volumes")
+# arguments are passed as parameter declarations to Option:'Project' is passed.
+@click.option('--project',default=None,
+help="Only Instances for project (tag Project:<name>)")
+def create_snapshot(project):
+    "Create snapshots for EC2 instances"
+    instances = filter_instances(project)
+    # for each instance,volume,create snapshot
+    for i in instances:
+        print("Stopping  {0}",format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("Creating snapshots of{0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+        print("Starting  {0}",format(i.id))
+        i.start()
+        i.wait_until_running()
+    print("Done")
+    return
+#command for 'List.
 @instances.command('list')
 # arguments are passed as parameter declarations to Option:'Project' is passed.
 @click.option('--project',default=None,
 help="Only Instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
-#project is passed to filter out and get list of instance
+    #project is passed to filter out and get list of instance
     instances = filter_instances(project)
-#iterate for getting instance collection sperated
-#eg:id,instancetype,state,state,dns,ProjectName
+    #iterate for getting instance collection sperated
+    #eg:id,instancetype,placement,state,dns,ProjectName
     for i in instances :
-# convert tag(see boto3 docs) as dictionary and iterate then print
+        # convert tag(see boto3 docs) as dictionary and iterate then print
         tags={t['Key']: t['Value'] for t in i.tags or []}
         print(','.join((
             i.id,
@@ -48,7 +123,7 @@ def list_instances(project):
 def stop_instances(project):
     "Stop EC2 instances"
     instances =filter_instances(project)
-# for each instance start function is invoked
+    # for each instance start function is invoked
     for i in instances:
         print("Stopping {0}...".format(i.id))
         i.stop()
@@ -62,11 +137,11 @@ def stop_instances(project):
 def start_instances(project):
     "start EC2 instances"
     instances =filter_instances(project)
-# for each instance stop function is invoked
+    # for each instance stop function is invoked
     for i in instances:
         print("Starting {0}...".format(i.id))
         i.start()
     return
 #invoke the group command
 if __name__ == '__main__':
-    instances()
+    cli()
